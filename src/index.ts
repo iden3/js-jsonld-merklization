994f3025e65be5a24f5d0a243d8d29348bb35435
Dimasik Kolezhniuk <D.Kolezhiniyk@gmail.com>
2023-06-23 17:16:51 +0200

    Feature/update-exports (#19)
    * Update exports & patch version
    
    * patch version
    
    * fix test

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { Merklizer } from './lib/merklizer';
 export { Path } from './lib/path';
 export { Hasher } from './lib/types/types';
 export { MerklizationConstants } from './lib/constants';
+export { getDocumentLoader } from './lib/options';
