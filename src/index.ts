88d8f456b99e4ffe65b40f04ec8489c46087468e
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2023-06-25 23:30:26 +0300

    add options to export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { Value } from './lib/types/types';
 export { MtValue } from './lib/mt-value';
 export { Merklizer } from './lib/merklizer';
 export { Path } from './lib/path';
-export { Hasher } from './lib/types/types';
+export { Hasher , Options} from './lib/types/types';
 export { MerklizationConstants } from './lib/constants';
 export { getDocumentLoader } from './lib/options';
