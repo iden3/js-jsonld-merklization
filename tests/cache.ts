8ce10c7359fbbbdd8b6c3c9682f5a4670530777c
Dimasik Kolezhniuk <d.kolezhiniyk@gmail.com>
2023-10-24 11:24:42 +0200

    Fix cache loader. Pass loader in test

--- tests/cache.ts
+++ tests/cache.ts
@@ -4,12 +4,12 @@ import { Options } from '../src/lib/types/types';
 import { W3C_CREDENTIAL_2018, W3C_VC_SCHEMA } from './data';
 import { DocumentLoader } from '../src/loaders/jsonld-loader';
 
-const cache = new Map<string, RemoteDocument>();
-cache.set(W3C_CREDENTIAL_2018, {
-  document: W3C_VC_SCHEMA,
-  documentUrl: W3C_CREDENTIAL_2018
-});
 export const cacheLoader = (opts?: Options): DocumentLoader => {
+  const cache = new Map<string, RemoteDocument>();
+  cache.set(W3C_CREDENTIAL_2018, {
+    document: W3C_VC_SCHEMA,
+    documentUrl: W3C_CREDENTIAL_2018
+  });
   return async (url): Promise<RemoteDocument> => {
     let remoteDoc = cache.get(url);
     if (remoteDoc) {
